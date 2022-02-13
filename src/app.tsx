acbd830df097d9c600c47514e5a512a61a8bb311
Fernando Daciuk <f.daciuk@gmail.com>
2022-02-13 20:29:36 -0300

    feat: use setInterval to verify if wallet is connected

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import {
   createWeb3Instance,
   createContract,
@@ -7,10 +7,23 @@ import {
 
 export function App () {
   const web3js = useRef<CryptoZombiesContract>()
+  const [userAccount, setUserAccount] = useState('')
 
   useEffect(() => {
-    web3js.current = createContract(createWeb3Instance())
-  }, [])
+    const web3Instance = createWeb3Instance()
+    web3js.current = createContract(web3Instance)
+    console.log('accounts.wallet', web3Instance.eth.accounts.wallet)
+
+    const id = setInterval(() => {
+      // TODO: Try to improve web3 types and remove `as any`
+      const account = (web3Instance.eth.accounts as any)[0]
+      if (account !== userAccount) {
+        setUserAccount(account)
+      }
+    }, 100)
+
+    return () => clearInterval(id)
+  }, [userAccount])
 
   return (
     <div>App</div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import { App } from '@/app'
 
 ReactDOM.render(
