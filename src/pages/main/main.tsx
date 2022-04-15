c19af6c6e9a500756adf46ea7a711d2ddd1204c4
Fernando Daciuk <f.daciuk@gmail.com>
2022-04-15 08:24:00 -0300

    feat: add address and score info

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,6 @@
 import { lazy, useEffect, Suspense } from 'react'
 import { useNavigate, Routes, Route, Link } from 'react-router-dom'
+import { Box } from '@chakra-ui/react'
 
 import { useAuth } from '@/resources'
 
@@ -11,7 +12,7 @@ const Battle = lazy(() => import('@/pages/battle'))
 const Fight = lazy(() => import('@/pages/fight'))
 
 export function Main () {
-  const { zombies } = useAuth()
+  const { address, zombies } = useAuth()
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -22,7 +23,7 @@ export function Main () {
 
   return (
     <>
-      <ul style={{ position: 'absolute', backgroundColor: '#222', padding: '20px', right: 0 }}>
+      <ul style={{ position: 'absolute', backgroundColor: '#222', padding: '20px', right: 0, bottom: 0 }}>
         <li><Link to='/'>Create Zombie</Link></li>
         <li><Link to='/army'>My Army</Link></li>
         <li><Link to='/army/123'>Zombie 123 details</Link></li>
@@ -30,6 +31,11 @@ export function Main () {
         <li><Link to='/battle'>Choose your opponent</Link></li>
         <li><Link to='/battle/234'>Fight with Zombie 234</Link></li>
       </ul>
+
+      <Box position='absolute' right='4' top='4' textAlign='right'>
+        <p>{address.substring(0, 10)}...</p>
+        <p>Score: 10000</p>
+      </Box>
 
       <Suspense fallback=''>
         <Routes>
